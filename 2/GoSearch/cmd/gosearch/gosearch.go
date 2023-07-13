5828877e3694058a170d587b859b357aac4a1424
Vitaly Goryachev <headbat@gmail.com>
2023-07-13 16:57:52 +0700

    code reorganisation

--- 2/GoSearch/cmd/gosearch/gosearch.go
+++ 2/GoSearch/cmd/gosearch/gosearch.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"gosearch/2/GoSearch/pkg/crawler/spider"
+	"gosearch/pkg/crawler/spider"
 	"log"
 )
 
@@ -15,31 +15,29 @@ func main() {
 		return
 	}
 
-	urls := [2]string{"https://go.dev", "https://golang.org"}
+	urls := [2]string{"hhtttps://go.dev", "https://golang.org"}
 	depth := 2
 
-	links, err := scanLinks(urls, depth)
-	if err != nil {
-		log.Printf("Error: %v", err)
-	}
+	links := scanLinks(urls, depth)
 
 	for _, l := range links {
 		fmt.Println(l)
 	}
 }
 
-func scanLinks(urls [2]string, depth int) ([]string, error) {
+func scanLinks(urls [2]string, depth int) []string {
 	spider := spider.New()
 	result := []string{}
 
 	for _, u := range urls {
 		doc, err := spider.Scan(u, depth)
 		if err != nil {
-			return nil, err
+			log.Print(err)
+			continue
 		}
 		for _, d := range doc {
 			result = append(result, d.URL)
 		}
 	}
-	return result, nil
+	return result
 }
